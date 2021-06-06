234c3bec83a26c323b76d4cb6ab6923e164789f6
Chris Gauthier <chris@elektrus.ca>
2021-06-06 14:42:18 -0500

    Added some comments that will help see my logic

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -14,12 +14,21 @@ class FormController extends Controller
 
     public function formSubmit(Request $request)
     {
+        //some error checking to see if we're getting the form request back
         print_r($request->all());
         print_r($request->cafftypes);
 
+        //validating the form with requirements
         $validated = $request->validate([
             'numofcups' => 'required|max:2|integer'
 
         ]);
+
+        //here is where i start with the database work.
+        //Connect to the database
+        //Select the caffene amounts and serving info from the db
+        // use this:   Max Caffeine (500) - (caf from db * servings)
+        //Then we're going to need some error checking here to ensure if the 
+        //required caffamount is not a negative number (if it is, we have went over our budget)
     }
 }
